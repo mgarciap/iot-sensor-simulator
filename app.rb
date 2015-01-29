b75c92dd61dd320e84deb9827ba2e44ec827a1f7
lcostantini <costantinileandro1@gmail.com>
2015-01-29 15:39:27 -0300

    Logs are displayed as a terminal.

--- app.rb
+++ app.rb
@@ -17,17 +17,21 @@ Cuba.define do
 
   on post do
     on "publish/:topic/:payload" do |topic, payload|
+      response = []
       begin
         unless CLIENT.connected?
           CLIENT.refresh_connection
-          res.write "Established connection to broker '#{CLIENT.broker}' and port '#{CLIENT.port}'\n"
+          response << "Established connection to broker '#{CLIENT.broker}' and port '#{CLIENT.port}'"
         end
         CLIENT.publish! payload, topic_out: topic
-        res.write "Published the value '#{payload}' in channel '#{topic}'"
+        response << "Published the value '#{payload}' in channel '#{topic}'"
+        res.write response
       rescue Timeout::Error
-        res.write "Connection timed out. Couldn't publish on broker"
+        response << "Connection timed out. Couldn't publish on broker"
+        res.write response
       rescue Exception => e
-        res.write "ERROR: #{e.message}"
+        response << "ERROR: #{e.message}"
+        res.write response
       end
     end
   end
