8fbef62705c38b9ced1e0a1b09f19e35a1ee6c9c
lcostantini <costantinileandro1@gmail.com>
2015-02-20 16:19:18 -0300

    Add img folder for cuba.

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require 'slim'
 require 'dotenv'
 require './helpers/logs_helpers'
 
-Cuba.use Rack::Static, urls: %w[/js /css], root: "public"
+Cuba.use Rack::Static, urls: %w[/js /css /img], root: "public"
 Cuba.plugin LogsHelpers
 Cuba.plugin Cuba::Render
 Cuba.settings[:render][:template_engine] = "slim"
